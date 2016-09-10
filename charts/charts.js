f00c6d848263762b9e79215278c065189b6e5a0b
shellchen <shell_chen@yeah.net>
2016-09-10 15:16:59 +0800

    chart object optimize

--- charts/charts.js
+++ charts/charts.js
@@ -24,144 +24,145 @@ function getUrlParams() {
     return result;
 }
 
-var Charts = function(elementId){
-    this.elementId = $(elementId);
-}
 
-Charts.prototype._parseParam=function(){
-    var params = getUrlParams();
-    var files = params["f"];
-    if(!files) {
-        return false;
-    }
-    return files
-}
+var Charts = function(element){
+    this.elementObject = $(element);
+    var thisChart = this;
 
-Charts.prototype.Draw = function(){
-    var files = this._parseParam();
-    if(files === false) return;
-    var url = 'datas/'+files;
-    var self = this;
-    $.getJSON(url, function(data){
-        var options = self._parseJson(data);
-        if(options === false) return;
-        self._drawCharts(options);
-    });
-}
-
-Charts.prototype._drawCharts = function(options){
-    var default_options = {
-        height: 700,
-        type: "line",
-        x_type: "linear",
-        x_title: "number",
-        yAxis: {
-            title: {text: 'number'},
-        },
-    };
-    var settings = $.extend({}, default_options, options);
-    this.elementId.highcharts({
-        chart:{
-            animation:false,
-            type: settings.type,
-            height: settings.height,
-        },
-        title:{
-            text: settings.title,
-        },
-        xAxis: {
-            type: settings.x_type,
-            title: {
-                text: settings.x_title,
-            },
-        },
-        yAxis: settings.yAxis,
-        credits: {
-            enabled: false
-        },
-        tooltip: {
-            shared: false,
-            formatter: function () {
-                var xAxis_type = this.series.xAxis.options.type;
-                var x  = this.x;
-                var y = this.y;
-                if(y !== parseInt(y)){
-                    y = y.toFixed(2);
-                }
-                if(xAxis_type === "datetime"){
-                    x = (new Date(this.x)).Format("yyyy-MM-dd hh:mm:ss");
-                }
-                var html = x+"<br>";
-                html += this.series.name +" " +y+ "<br>";
-                if(this.point.Datas){
-                    html += this.point.Datas;
-                }
-                return html;
-            }
-        },
-        navigation: {
-            buttonOptions: {
-                enabled: false
-            }
-        },
-        series: settings.series,
-    });
-}
-
-Charts.prototype._parseJson = function(datas){
-    var option = datas.option;
-    var y_label = option.y_label;
-    var y_axis = {title: {text: ''}};
-    if(!$.isArray(y_label)){
-        y_label = [y_label];
-    }
-    var yAxises = [];
-    $.each(y_label, function(index, y_l){
-        var res = {title: {text: y_l}};
-        if(index === 1){
-            res.opposite = true;
-        }
-        yAxises.push(res);
-    });
-
-    var series = [];
-    for(var name in datas.data){
-        var d = datas.data[name];
-        var yAxisIndex = d.y_axis===1?d.y_axis:0;
-        if(yAxisIndex >= yAxises.length){
-            alert('y_label 错误');
+    var _parseParam = function(){
+        var params = getUrlParams();
+        var files = params["f"];
+        if(!files) {
             return false;
         }
-        var seriesData = d.values.map(function(vv){
-            return {
-                x: vv[0],
-                y: vv[1],
-                Datas: vv[2],
+        return files
+    }
+
+    var _parseJson = function(datas){
+        var option = datas.option;
+        var y_label = option.y_label;
+        var y_axis = {title: {text: ''}};
+        if(!$.isArray(y_label)){
+            y_label = [y_label];
+        }
+        var yAxises = [];
+        $.each(y_label, function(index, y_l){
+            var res = {title: {text: y_l}};
+            if(index === 1){
+                res.opposite = true;
             }
+            yAxises.push(res);
         });
-        series.push({
-            name: name,
-            data: seriesData,
-            type: 'line',
-            yAxis: yAxisIndex,
+
+        var series = [];
+        for(var name in datas.data){
+            var d = datas.data[name];
+            var yAxisIndex = d.y_axis===1?d.y_axis:0;
+            if(yAxisIndex >= yAxises.length){
+                alert('y_label 错误');
+                return false;
+            }
+            var seriesData = d.values.map(function(vv){
+                return {
+                    x: vv[0],
+                    y: vv[1],
+                    Datas: vv[2],
+                }
+            });
+            series.push({
+                name: name,
+                data: seriesData,
+                type: 'line',
+                yAxis: yAxisIndex,
+            });
+        }
+
+        var chart_options = {
+            title: option.title,
+            series: series,
+            yAxis: yAxises,
+            x_title: option.x_label,
+        };
+        if(["time", "datetime"].indexOf(option.x_type) != -1){
+            chart_options["x_type"] = "datetime";
+        }
+        if(option.height){
+            chart_options["height"] = option.height;
+        }
+        return chart_options;
+    }
+
+    this._drawCharts = function(options){
+        var default_options = {
+            height: 700,
+            type: "line",
+            x_type: "linear",
+            x_title: "number",
+            yAxis: {
+                title: {text: 'number'},
+            },
+        };
+        var settings = $.extend({}, default_options, options);
+        this.elementObject.highcharts({
+            chart:{
+                animation:false,
+                type: settings.type,
+                height: settings.height,
+            },
+            title:{
+                text: settings.title,
+            },
+            xAxis: {
+                type: settings.x_type,
+                title: {
+                    text: settings.x_title,
+                },
+            },
+            yAxis: settings.yAxis,
+            credits: {
+                enabled: false
+            },
+            tooltip: {
+                shared: false,
+                formatter: function () {
+                    var xAxis_type = this.series.xAxis.options.type;
+                    var x  = this.x;
+                    var y = this.y;
+                    if(y !== parseInt(y)){
+                        y = y.toFixed(2);
+                    }
+                    if(xAxis_type === "datetime"){
+                        x = (new Date(this.x)).Format("yyyy-MM-dd hh:mm:ss");
+                    }
+                    var html = x+"<br>";
+                    html += this.series.name +" " +y+ "<br>";
+                    if(this.point.Datas){
+                        html += this.point.Datas;
+                    }
+                    return html;
+                }
+            },
+            navigation: {
+                buttonOptions: {
+                    enabled: false
+                }
+            },
+            series: settings.series,
         });
     }
 
-    var chart_options = {
-        title: option.title,
-        series: series,
-        yAxis: yAxises,
-        x_title: option.x_label,
-    };
-    if(["time", "datetime"].indexOf(option.x_type) != -1){
-        chart_options["x_type"] = "datetime";
+
+    this.Draw = function(){
+        var files = _parseParam();
+        if(files === false) return;
+        var url = 'datas/'+files;
+        $.getJSON(url, function(data){
+            var options = _parseJson(data);
+            if(options === false) return;
+            thisChart._drawCharts(options);
+        });
     }
-    if(option.height){
-        chart_options["height"] = option.height;
-    }
-    return chart_options;
 }
-
 
 Date.prototype.Format = function (fmt) {
     var o = {
